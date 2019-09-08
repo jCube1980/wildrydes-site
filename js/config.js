ecc9bd0e037032fdbfcbee33b370897fbdec455d
Joseph Justus <joseph.justus@gmail.com>
2019-09-08 14:16:18 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_cN5h1oJAh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '38o9t5ph0qutj77q38on3q9md7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
